b57ececc533bfbe2ddfb0830b95b9c929bb944a8
Vital D <netmin@pm.me>
2023-10-27 12:28:52 +0500

    fix configuration db

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool =
-        PgPoolOptions::new().connect_lazy_with(configuration.database.without_db());
+        PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
